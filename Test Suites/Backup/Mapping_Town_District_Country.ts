456b59fa4a8a4c2a6195007bcfae49809eb9427d
I1675 <I1675@IND-CHN-LT11400>
2024-07-31 23:16:43 +0530

    Final_Code_commit_31/07/24

---
+++ Test Suites/Backup/Mapping_Town_District_Country.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Mapping_Town_District_Country</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d194b2d6-434f-4bb5-9240-c0329dbd02cf</testSuiteGuid>
+   <testCaseLink>
+      <guid>4761a7ea-9c10-4290-b6a5-d3295e29a697</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/BNPL/Finweb/LoanApp_Create_Lead</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>49dd2c89-a29c-4d06-962d-76fa32d5c382</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/BNPL/Finweb/Mapping_Town_District_Country</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
